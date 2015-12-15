3db5a619b47d586beb992ca504a5614dfc4f0e06
jkl94 <jax0503@gmail.com>
2015-12-15 19:27:54 +0100

    v3

--- GiocoDellaVita/Generator.jl
+++ GiocoDellaVita/Generator.jl
@@ -185,7 +185,7 @@ type Generator
 			#funkcja generujaca n następnych boardów zapis co liczbę saving
 		function generateNOnes( board :: Board, infinity :: Bool, n :: Int, saving :: Int) 
 			bIO = BoardIO()
-			z = saving
+			z = 0
 			if( infinity == false)
 				for ( i = 1 : n)
 					checkingWithBorders( board )
