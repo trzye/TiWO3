be4c130ae32f4bf1135fdb1a346207d6232d51bf
jkl94 <jax0503@gmail.com>
2015-12-15 05:03:25 +0100

    example do generatora

---
+++ GiocoDellaVita/generatorExample.jl
@@ -0,0 +1,10 @@
+include("Board.jl")
+include("Generator.jl")
+
+#Utworzenie instancji do zapisywania/wczytywania planszy z pliku.
+bIO = BoardIO()
+
+#Wczytanie planszy z pliku
+board = bIO.loadFromFile("daneTestowe.txt")
+gen = Generator()
+gen.generateNextOne(board,false)
