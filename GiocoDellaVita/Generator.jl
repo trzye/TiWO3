c7666b06e61c342c8fc8bb8ebb85d5a38ae91974
jkl94 <jax0503@gmail.com>
2016-01-03 23:47:28 +0100

    v4.1
    dodanie funkcji tworzącej nową tablice i kopiującej do niej podaną tablice
    znalezienie błędu w przeszukiwaniu

--- GiocoDellaVita/Generator.jl
+++ GiocoDellaVita/Generator.jl
@@ -1,8 +1,24 @@
 include("Board.jl")
 
+#funkcja kopiująca tworzaca i kopujaca tablice
+function copy(board :: Board)
+	w = board.width
+	h = board.hight
+	board2 = Board(h,w)
+	cells1 = board.cells
+	cells2 = board2.cells
+
+	for i=1:h
+		for j=1:w
+			cells2[i , j]= cells1[i , j]
+		end
+	end
+	return board2
+end
+
 #funkcja realizujaca zasady gry w ĹĽycie
-function rules( b :: Board, i:: Int, j:: Int, n :: Int)
-	if ( b.cells[i,j].isAlife == true)
+function rules(bo :: Board, b :: Board, i:: Int, j:: Int, n :: Int)
+	if ( bo.cells[i,j].isAlife == true)
 		if (( n == 2) || ( n == 3))
 			b.cells[i,j].isAlife = true
 		else
@@ -20,16 +36,16 @@ end
 
 
 #funkcja spradzajÄ…ca sasiadĂłw zadanej komorki[i,j], parametry mnop -(-1,0,1) z granicami
-function check( b :: Board, i :: Int, j :: Int, m :: Int, n :: Int, o :: Int, p :: Int )
-	tmp = 0
-	for( k  = i + m : i + o)
-		for( l = j + n : j + p)
-			if (( k != i ) && (l != j ) && ( b.cells[k,l].isAlife == true) )
+function check( bo :: Board, b :: Board, i :: Int, j :: Int, m :: Int, n :: Int, o :: Int, p :: Int )
+	tmp = 1
+	for( k  = i + m : i + n)
+		for( l = j + o : j + p)
+			if (( k != i ) && (l != j ) && ( bo.cells[k,l].isAlife == true) )
 				tmp = tmp +1
 			end
 		end
 	end
-	board = rules( b, i, j, tmp)
+	board = rules( bo, b, i, j, tmp)
 	return board
 end
 
@@ -63,34 +79,35 @@ function check2(bo :: Board, tmp :: Int, a :: Int, b :: Int, c :: Int, d :: Int,
 end
 
 #funkcja przejsc po tablicy z granicami
-function checkingWithBorders( b :: Board )
-	h = b.hight
-	w = b.width
+function checkingWithBorders( bo :: Board )
+	h = bo.hight
+	w = bo.width
+	b = copy(bo)
 	for i =1 : h
 		for j = 1 : w
 			if( i == 1)
 				if( j == 1)
-					b = check(b, i, j, 0, 0, 1, 1)
+					b = check(bo, b, i, j, 0, 1, 0, 1)
 				elseif ( j == w)
-					b = check(b, i, j, 0, -1, 1, 0)
+					b = check(bo, b, i, j, 0, 1, -1, 0)
 				else
-					b = check(b, i, j, 0, -1, 1, 1)
+					b = check(bo, b, i, j, 0, 1, -1, 1)
 				end
 			elseif ( i == h)
 				if( j == 1)
-					b = check(b, i, j, -1, 0, 0, 1)
+					b = check(bo, b, i, j, -1, 0, 0, 1)
 				elseif ( j == w)
-					b = check(b, i, j, -1, -1, 0, 0)
+					b = check(bo, b, i, j, -1, 0, -1, 0)
 				else
-					b = check(b, i, j, -1, -1, 0, 1)
+					b = check(bo, b, i, j, -1, 0, -1, 1)
 				end
 			else
 				if( j == 1)
-					b = check(b, i, j, -1, 0, 1, 1)
+					b = check(bo, b, i, j, -1, 1, 0, 1)
 				elseif ( j == w)
-					b = check(b, i ,j, -1, -1, 1, 0)
+					b = check(bo, b, i ,j, -1, 1, -1, 0)
 				else
-					b = check(b, i, j, -1, -1, 1, 1)
+					b = check(bo, b, i, j, -1, 1, -1, 1)
 				end
 			end
 		end
@@ -99,76 +116,77 @@ function checkingWithBorders( b :: Board )
 end
 
 #funkcja przejsc po tablicy bez granic
-function checkingInfinity(b :: Board)
-	h = b.hight
-	w = b.width
-	for i = 1 :h -1
+function checkingInfinity(bo :: Board)
+	h = bo.hight
+	w = bo.width
+	b = copy(bo)
+	for i = 1 :h
 		for j = 1 : w
-			tmp = 0
+			tmp = 1
 			if ((i == 1) && (j != 1)&& (j != w) )
 				for( l = j - 1 : j + 1 )
-					if( b.cells[h,l].isAlife == true )
+					if( bo.cells[h,l].isAlife == true )
 						tmp = tmp +1
 					end
-					if ( (l != j ) && ( b.cells[i,l].isAlife == true ) )
+					if ( (l != j ) && ( bo.cells[i,l].isAlife == true ) )
 						tmp = tmp +1
 					end
-					if (b.cells[i+1,l].isAlife == true )
+					if (bo.cells[i+1,l].isAlife == true )
 						tmp = tmp +1
 					end
 				end
 			elseif( (i == h)&&(j != 1)&&(j != w) )
 				for( l = j -1 : j + 1)
-					if( b.cells[1,l].isAlife == true )
+					if( bo.cells[1,l].isAlife == true )
 						tmp = tmp +1
 					end
-					if ( (l != j ) && ( b.cells[i,l].isAlife == true ))
+					if ( (l != j ) && ( bo.cells[i,l].isAlife == true ))
 						tmp = tmp +1
 					end
-					if (b.cells[i-1,l].isAlife == true)
+					if (bo.cells[i-1,l].isAlife == true)
 						tmp = tmp +1
 					end
 				end
-			elseif( (j == 1) && ( i != 1) && (i != w) )
+			elseif( (j == 1) && ( i != 1) && (i != h) )
 				for(k = i -1 : i + 1)
-					if( b.cells[k,w].isAlife == true)
+					if( bo.cells[k,w].isAlife == true)
 						tmp = tmp + 1
 					end
 					for(l = j : j+1)
-						if( b.cells[k,l].isAlife == true)
+						if( bo.cells[k,l].isAlife == true)
 							tmp = tmp +1
 						end
 					end
 				end
-			elseif( (j == w) && ( i != 1) && (i != w))
+			elseif( (j == w) && ( i != 1) && (i != h))
 				for(k = i -1 : i + 1)
-					if( b.cells[k,1].isAlife == true)
+					if( bo.cells[k,1].isAlife == true)
 						tmp = tmp + 1
 					end
 					for(l = j -1 : j)
-						if( b.cells[k,l].isAlife == true)
+						if( bo.cells[k,l].isAlife == true)
 							tmp = tmp +1
 						end
 					end
 				end
 			elseif((i == 1) && (j == 1))
-				tmp = check2(b, tmp, h, w, h, j, h, j+1, i, w, i, j, i, j+1, i+1, w, i+1, j, i+1, j+1)	
+				tmp = check2(bo, tmp, h, w, h, j, h, j+1, i, w, i, j, i, j+1, i+1, w, i+1, j, i+1, j+1)	
 			elseif((i == 1) && (j == w))
-				tmp = check2(b, tmp, h, j-1, h, j, h, 1, i, j-1, i, j, i, 1, i+1, j-1, i+1, j, i+1, 1)
+				tmp = check2(bo, tmp, h, j-1, h, j, h, 1, i, j-1, i, j, i, 1, i+1, j-1, i+1, j, i+1, 1)
 			elseif((i == h) && (j == 1))
-				tmp = check2(b, tmp, i-1, w, i-1, j, i-1, j+1, i, w, i, j, i, j+1, 1, w, 1, j, 1, j+1)
+				tmp = check2(bo, tmp, i-1, w, i-1, j, i-1, j+1, i, w, i, j, i, j+1, 1, w, 1, j, 1, j+1)
 			elseif((i == h) && (j == w))
-				tmp = check2(b, tmp, i-1, j-1, i-1, j, i-1, 1, i, j-1, i, j, i, 1, 1, j-1, 1, j, 1, 1)
+				tmp = check2(bo, tmp, i-1, j-1, i-1, j, i-1, 1, i, j-1, i, j, i, 1, 1, j-1, 1, j, 1, 1)
 			elseif((i != 1) && (i != h) &&( j != 1) && (j != w) )
 				for( k  = i -1 : i + 1)
 					for( l = j -1 : j + 1)
-						if (( k != i ) && (l != j ) && ( b.cells[k,l].isAlife == true) )
+						if (( k != i ) && (l != j ) && ( bo.cells[k,l].isAlife == true) )
 							tmp = tmp +1
 						end
 					end
 				end
 			end
-			b = rules(b,i,j,tmp)
+			b = rules(bo, b,i,j,tmp)
 		end
 	end
 	return b
@@ -181,7 +199,7 @@ type Generator
 	Generator() =
 		new(   #funkcja generujaca jeden nastÄ™pny board
 		function generateNextOne( board :: Board, infinity :: Bool) 
-			bIO = BoardIO()
+			#bIO = BoardIO()
 			if( infinity == false)
 				nb = checkingWithBorders( board )
 				return nb
@@ -204,6 +222,8 @@ type Generator
 			end
 			fill!(nOnes, empty)
 			z = 1
+			nOnes[1,1] = board
+
 			if( infinity == false)
 				for ( i = 1 : n)
 					board = checkingWithBorders( board )
@@ -223,6 +243,7 @@ type Generator
 					end
 				end 
 			end 
+
 			return nOnes
 		end
 		)
