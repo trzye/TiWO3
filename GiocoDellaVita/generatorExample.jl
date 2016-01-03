a4a7d415d1a3ce1c99c9841202c24219c1e54ad5
jkl94 <jax0503@gmail.com>
2016-01-03 23:49:18 +0100

    v4.1
    dodanie przykładu wykorzystania funkcji kupującej

--- GiocoDellaVita/generatorExample.jl
+++ GiocoDellaVita/generatorExample.jl
@@ -10,16 +10,34 @@ gen = Generator()
 printBoard(board)
 print('\n')
 
-#Jedna generacja na planszy z granicami.
+#wywołanie funkcji tworzącej i kopiującej podany board do nowej 
+boo = copy(board)
+printBoard(boo)
+print('\n')
+
+
+#generacja jednego na planszy z granicami
 next = gen.generateNextOne(board,false)
 printBoard(next)
+print('\n')
 
-#Jedna generacja na planszy bez granic.
+#generacja jednego na planszy bez granic
 n = gen.generateNextOne(board,true)
 printBoard(n)
+print('\n')
 
-#Generacja wielu (trzech) iteracji. Zapisanie co jedną.  Plansza z granicami.
+#generacja wielu 3 iteracje zapisane co 1 z granicami
 nn = gen.generateNOnes(board,false, 3, 1)
+	for i = 1 : 4
+		printBoard(nn[i,1])
+		print('\n')
+	end
+print('\n')
 
-#Generacja wielu (trzech) iteracji. Zapisanie co jedną. Plansza bez granic.
-gen.generateNOnes(board,true, 3, 1)
+#generacja wielu 3 iteracje zapisane co 1 bez granic
+nnn = gen.generateNOnes(board,true, 3, 1)
+	for i = 1 : 4
+		printBoard(nnn[i,1])
+		print('\n')
+	end
+print('\n')
